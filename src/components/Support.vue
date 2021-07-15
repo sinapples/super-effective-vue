8ce3ef3a2737d040b5174ed4662a8be55ef33f32
sinapples <Josh.lopez.is@gmail.com>
2021-07-15 00:06:53 -0400

    fixed padding

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="secondary " dark tile>
+  <v-card color="secondary px-0" dark tile>
     <v-sheet color="primary  ">
       <v-card-title class="justify-center text-h7  white--text">
         <div class="justify-center">
@@ -9,33 +9,35 @@
       </v-card-title>
     </v-sheet>
 
-    <v-card-text class=" body-1 text-center ">
-      <v-container>
+    <v-card-text class=" body-1 text-center px-0 ">
+      <v-container class="px-0">
         <v-row align="center" justify="center" no-gutters>
-          <v-col cols="12">
-            <v-col cols="auto">
-              <v-list-item-avatar class="ma-3" size="125" right rounded>
-                <v-img
-                  src="https://firebasestorage.googleapis.com/v0/b/pockets-check.appspot.com/o/1C377174-3F9A-4F03-B42A-C1E68C5855C5.JPG?alt=media&token=f086d377-a289-4db9-a6f1-5436d4df55fc"
-                ></v-img>
-              </v-list-item-avatar>
-            </v-col>
-            <v-col>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-title class="text-start text-h5 mb-2">
-                    Hi, I'm Josh!
-                  </v-list-item-title>
-                  <div class=" text-start  body-1">
-                    I hope you found
-                    <strong class="primary--text"> {{ appTitle }}</strong>
-                    helpful!
-                  </div>
-                  <div class=" text-start  body-1"></div>
-                </v-list-item-content>
-              </v-list-item>
-            </v-col>
+          <v-spacer />
+
+          <v-col cols="auto">
+            <v-list-item-avatar class="ma-3" size="125" right rounded>
+              <v-img
+                src="https://firebasestorage.googleapis.com/v0/b/pockets-check.appspot.com/o/1C377174-3F9A-4F03-B42A-C1E68C5855C5.JPG?alt=media&token=f086d377-a289-4db9-a6f1-5436d4df55fc"
+              ></v-img>
+            </v-list-item-avatar>
           </v-col>
+          <v-col>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-start text-h5 mb-2">
+                  Hi, I'm Josh!
+                </v-list-item-title>
+                <div class=" text-start  body-1">
+                  I hope you found
+                  <strong class="primary--text"> {{ appTitle }}</strong>
+                  helpful!
+                </div>
+                <div class=" text-start  body-1"></div>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+
+          <v-spacer />
         </v-row>
       </v-container>
       <div class="white--text">
@@ -55,7 +57,7 @@
       </div>
     </v-card-text>
 
-    <v-card-actions class="pb-8">
+    <v-card-actions class="pb-8 pl-1">
       <v-spacer />
       <v-btn
         color="blue lighten-2"
